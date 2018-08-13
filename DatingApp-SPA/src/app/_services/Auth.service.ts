a48107b0b71a99cc1bd0700d50793358dbf58b8b
Mohammed.Tawfik <m.abdelraza2@gmail.com>
2018-08-13 18:40:42 +0200

    Adding Auth0/angular-jwt Adding isUserLoggedIn() method

--- DatingApp-SPA/src/app/_services/Auth.service.ts
+++ DatingApp-SPA/src/app/_services/Auth.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '../../../node_modules/@angular/common/http';
 import { User } from '../_models/user';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'https://localhost:7001/api/auth/';
+  jwtHelper = new JwtHelperService();
+
   constructor(private http: HttpClient) {}
 
   login(user: User) {
@@ -23,5 +26,11 @@ export class AuthService {
 
   register(user: User) {
     return this.http.post(this.baseUrl + 'register', user);
+  }
+
+  isUserLoggedIn() {
+    const token = localStorage.getItem('token');
+    this.jwtHelper.tokenGetter = () => localStorage.getItem('token');
+    return  token != null && !this.jwtHelper.isTokenExpired();
   }
 }
