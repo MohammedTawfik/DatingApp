c7075a67c4646bb9dcc611df0cd113f783e3f65c
Mohammed.Tawfik <m.abdelraza2@gmail.com>
2018-08-31 20:30:39 +0200

    -Handling errors globally in Startup.cs

---
+++ DatingApp.API/Helpers/HttpResponseExtension.cs
@@ -0,0 +1,12 @@
+namespace Microsoft.AspNetCore.Http
+{
+    public static class HttpResponseExtension
+    {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-Error",message);
+            response.Headers.Add("Access-Control-Expose-Header","Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin","*");
+        }
+    }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,9 +1,12 @@
-﻿using AutoMapper;
+﻿using System.Net;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Data.Repos;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -56,7 +59,18 @@ namespace DatingApp.API
             }
             else
             {
-                app.UseHsts();
+                app.UseExceptionHandler(builder=>{
+                    builder.Run(async context=>{
+                        context.Response.StatusCode=(int)HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+                //app.UseHsts();
             }
             //seeder.SeedUsers();
             app.UseCors(origin=>origin.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
