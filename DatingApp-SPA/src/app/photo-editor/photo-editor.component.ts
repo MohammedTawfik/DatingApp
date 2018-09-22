9b2224c05151b433c994fb82241c49bcf33f29a3
Mohammed.Tawfik <m.abdelraza2@gmail.com>
2018-09-22 09:26:00 +0200

    fix tsline errors

--- DatingApp-SPA/src/app/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/photo-editor/photo-editor.component.ts
@@ -54,8 +54,7 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
-        if(photo.isMain)
-        {
+        if (photo.isMain) {
           this.authService.changeUserPhoto(photo.url);
         this.authService.currentUser.photoUrl = photo.url;
         localStorage.setItem('currentUser', JSON.stringify(this.authService.currentUser));
