cd3c63ad2e76308d47a61ef6feb39450ed526bb6
Mohammed.Tawfik <m.abdelraza2@gmail.com>
2018-09-29 19:50:29 +0200

    -Adding PagedList class -Adding PaginationHeader -Adding AddPaginationHeader method

--- DatingApp.API/Helpers/HttpResponseExtension.cs
+++ DatingApp.API/Helpers/HttpResponseExtension.cs
@@ -1,12 +1,24 @@
+using DatingApp.API.Helpers;
+using Newtonsoft.Json;
+
 namespace Microsoft.AspNetCore.Http
 {
     public static class HttpResponseExtension
     {
         public static void AddApplicationError(this HttpResponse response, string message)
         {
-            response.Headers.Add("Application-Error",message);
-            response.Headers.Add("Access-Control-Expose-Header","Application-Error");
-            response.Headers.Add("Access-Control-Allow-Origin","*");
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Header", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static void AddPaginationHeader(this HttpResponse response,
+         int totalPages, int totalCount, int pageSize, int currentPage)
+        {
+            PaginationHeader header = new PaginationHeader(currentPage, pageSize, totalCount, totalPages);
+            string jsonHeader = JsonConvert.SerializeObject(header);
+            response.Headers.Add("Pagination", jsonHeader);
+            response.Headers.Add("Access-Control-Expose-Header", "Application-Error");
         }
     }
 }
